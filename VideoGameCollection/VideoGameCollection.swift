e4b900409ad3fb7937e3f49c8fd22886a6e3d2ca
Jonathon Lannon <jonathonlannon@icloud.com>
2021-07-09 18:02:42 -0400

    Creation of initial data model file

---
+++ VideoGameCollection/VideoGameCollection.swift
@@ -0,0 +1,12 @@
+//
+//  VideoGameCollection.swift
+//  VideoGameCollection
+//
+//  Created by Jonathon Lannon on 7/9/21.
+//
+
+import Foundation
+
+class VideoGameCollection: ObservableObject{
+    
+}
