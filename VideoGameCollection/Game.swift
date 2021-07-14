64dc07d0b2c15eb617fdb024ac7df4e7ca42f628
Jonathon Lannon <jonathonlannon@icloud.com>
2021-07-13 21:18:42 -0400

    Add comments/documentation to all Game and Platform structs

--- VideoGameCollection/Game.swift
+++ VideoGameCollection/Game.swift
@@ -5,23 +5,35 @@
 //  Created by Jonathon Lannon on 7/9/21.
 //
 
+//import the following resources...
 import Foundation
 
+/**
+ GameResults: struct used when encoding/decoding JSON search results for the adding games through the AddGameView
+ Note: this struct should not be used when processing actual results, it is only meant as a container to hold them for API decoding purposes
+ Results should be used out of the "results" array, which is full of game information
+ Reference the RAWG online API documentation for more information
+ */
 struct GameResults: Codable{
-    var count: Int
-    var results: [GameSearch]
+    var count: Int  //number of results returned
+    var results: [GameSearch]   //array of game data
     
     init() {
+        //initialize both to just be essentially "empty" properties (0 and [] respectively)
         self.count = 0
         self.results = []
     }
 }
 
+/**
+ GameSearch: struct to be used when actually reading game data results when searching for games through the AddGameView
+ Reference the RAWG online API documentation for more information about parameters
+ */
 struct GameSearch: Codable, Identifiable{
     enum codingKey: CodingKey {
         case id, name, platforms
     }
-    var id: Int
-    let name: String
-    let platforms: [PlatformSearchResult]
+    var id: Int     //id of the game
+    let name: String    //name of the game
+    let platforms: [PlatformSearchResult]   //platforms the game supports
 }
--- VideoGameCollection/Platform.swift
+++ VideoGameCollection/Platform.swift
@@ -5,26 +5,42 @@
 //  Created by Jonathon Lannon on 7/10/21.
 //
 
+//import the following frameworks...
 import Foundation
 
+/**
+ PlatformSearchResult: struct for decoding JSON platform data for when a user searches for a game
+ This struct contains the results for when a user searches for a game for what platforms this game is on
+ Actual platform data is stored, but is not readable through this struct, as that is the Platform struct
+ */
 struct PlatformSearchResult: Codable, Hashable{
     var released_at: String?
-    var platform: Platform
+    var platform: Platform      //platform the game was released on
     
+    //hash function for making this function compliant with the Hashable protocol
     func hash(into hasher: inout Hasher) {
         hasher.combine(released_at)
         hasher.combine(platform)
     }
 }
 
+/**
+ PlatformSelection: struct to be used when decoding JSON data for the platform filter picker for the AddGameView screen
+ This is only to be used for that picker's data, and nothing else
+ */
 struct PlatformSelection: Codable{
     var results: [Platform]
 }
 
+/**
+ Platform: struct that holds data for the platform a game is on
+ This is primarily used when decoding data in the AddGameView
+ */
 struct Platform: Codable, Hashable{
-    var id: Int
-    var name: String
+    var id: Int     //id of the platform
+    var name: String    //name of the platform
     
+    //hash function for making this function compliant with the Hashable protocol
     func hash(into hasher: inout Hasher) {
         hasher.combine(id)
         hasher.combine(name)
