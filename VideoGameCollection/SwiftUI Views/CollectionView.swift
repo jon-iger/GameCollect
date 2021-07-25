e87891046efeaef35cc677179f5f59b213faf2b8
Jonathon Lannon <jonathonlannon@icloud.com>
2021-07-25 13:21:45 -0400

    Add basic grid view to CollectionView.swift.

--- VideoGameCollection/SwiftUI Views/CollectionView.swift
+++ VideoGameCollection/SwiftUI Views/CollectionView.swift
@@ -12,6 +12,7 @@ struct CollectionView: View {
     @State var searchText = String()    //string used for holding the user's current search text
     @State var activeSearch = false
     @State var searchResults: [Int] = []
+    @State var gridView = false
     var body: some View {
         let bindSearch = Binding<String>(
             //display displayText for the user to see
@@ -36,89 +37,96 @@ struct CollectionView: View {
         )
         NavigationView{
             VStack{
-                List{
-                    HStack{
-                        Image(systemName: "magnifyingglass")
-                            .padding(4)
-                        TextField("Search", text: bindSearch)
-                            .onTapGesture {
-                                activeSearch = true
-                            }
-                    }
-                    if !activeSearch{
-                        ForEach(Array(gameObject.gameCollection), id: \.self){ game in
-                            GameCollectionRow(id: game.id)
+                if !gridView{
+                    List{
+                        HStack{
+                            Image(systemName: "magnifyingglass")
+                                .padding(4)
+                            TextField("Search", text: bindSearch)
+                                .onTapGesture {
+                                    activeSearch = true
+                                }
                         }
-                        .onDelete(perform: deleteGame)
-                    }
-                    else{
-                        ForEach(searchResults, id: \.self){ game in
-                            GameCollectionRow(id: game)
+                        if !activeSearch{
+                            ForEach(Array(gameObject.gameCollection), id: \.self){ game in
+                                GameCollectionRow(id: game.id)
+                            }
+                            .onDelete(perform: deleteGame)
+                        }
+                        else{
+                            ForEach(searchResults, id: \.self){ game in
+                                GameCollectionRow(id: game)
+                            }
                         }
                     }
                 }
-                .navigationBarTitle("Game Collection")
-                .navigationBarItems(leading: EditButton(), trailing:
-                                        Menu{
-                                            Section{
-                                                Button{
-                                                    print("List View")
-                                                }
-                                                label:{
-                                                    Image(systemName: "list.bullet")
-                                                    Text("List View")
-                                                }
-                                                Button{
-                                                    print("Grid View")
-                                                }
-                                                label:{
-                                                    Image(systemName: "square.grid.2x2")
-                                                    Text("Grid View")
-                                                }
-                                            }
-                                            Section{
-                                                Button{
-                                                    sortByDate()
-                                                }
-                                                label:{
-                                                    Image(systemName: "clock")
-                                                    Text("Recently Added")
-                                                }
-                                                Button{
-                                                    print("Hi")
-                                                }
-                                                label:{
-                                                    Image(systemName: "gamecontroller")
-                                                    Text("Platform")
-                                                }
-                                                Button{
-                                                    sortByTitle()
-                                                }
-                                                label:{
-                                                    Image(systemName: "abc")
-                                                    Text("Title")
-                                                }
-                                            }
-                                        } label:{
-                                            Image(systemName: "ellipsis.circle")
-                                        }
-                )
+                else{
+                    ForEach(Array(gameObject.gameCollection), id: \.self){ game in
+                        GameCollectionGrid(id: game.id)
+                    }
+                }
             }
+            .navigationBarTitle("Game Collection")
+            .navigationBarItems(leading: EditButton(), trailing:
+                                    Menu{
+                                        Section{
+                                            Button{
+                                                gridView = false
+                                            }
+                                            label:{
+                                                Image(systemName: "list.bullet")
+                                                Text("List View")
+                                            }
+                                            Button{
+                                                gridView = true
+                                            }
+                                            label:{
+                                                Image(systemName: "square.grid.2x2")
+                                                Text("Grid View")
+                                            }
+                                        }
+                                        Section{
+                                            Button{
+                                                sortByDate()
+                                            }
+                                            label:{
+                                                Image(systemName: "clock")
+                                                Text("Recently Added")
+                                            }
+                                            Button{
+                                                print("Hi")
+                                            }
+                                            label:{
+                                                Image(systemName: "gamecontroller")
+                                                Text("Platform")
+                                            }
+                                            Button{
+                                                sortByTitle()
+                                            }
+                                            label:{
+                                                Image(systemName: "abc")
+                                                Text("Title")
+                                            }
+                                        }
+                                    } label:{
+                                        Image(systemName: "ellipsis.circle")
+                                    }
+            )
             GameDetailsView(id: UserDefaults.standard.integer(forKey: "lastViewedGame"))
         }
     }
-        func deleteGame(at offsets: IndexSet) {
-            gameObject.gameCollection.remove(atOffsets: offsets)
-            VideoGameCollection.saveToFile(basicObject: gameObject)
-        }
-        func sortByTitle(){
-            gameObject.gameCollection.sort(by: {$0.title < $1.title})
-            VideoGameCollection.saveToFile(basicObject: gameObject)
-        }
-        func sortByDate(){
-            gameObject.gameCollection.sort(by: {$0.dateAdded > $1.dateAdded})
-            VideoGameCollection.saveToFile(basicObject: gameObject)
-        }
+    func deleteGame(at offsets: IndexSet) {
+        gameObject.gameCollection.remove(atOffsets: offsets)
+        VideoGameCollection.saveToFile(basicObject: gameObject)
+    }
+    func sortByTitle(){
+        gameObject.gameCollection.sort(by: {$0.title < $1.title})
+        VideoGameCollection.saveToFile(basicObject: gameObject)
+    }
+    func sortByDate(){
+        gameObject.gameCollection.sort(by: {$0.dateAdded > $1.dateAdded})
+        VideoGameCollection.saveToFile(basicObject: gameObject)
+    }
 }
 
 struct CollectionView_Previews: PreviewProvider {
