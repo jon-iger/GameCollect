daa49a33e2c5043a6fd7df8d28e4950c01d833a8
Jonathon Lannon <jonathonlannon@icloud.com>
2021-08-07 20:17:41 -0400

    Remove local persistence container.

--- VideoGameCollection/VideoGameCollectionApp.swift
+++ VideoGameCollection/VideoGameCollectionApp.swift
@@ -9,12 +9,10 @@ import SwiftUI
 
 @main
 struct VideoGameCollectionApp: App {
-    let persistenceController = PersistenceController.shared
     let gameCollection: VideoGameCollection = VideoGameCollection.loadFromFile()
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
                 .environmentObject(gameCollection)
         }
     }
