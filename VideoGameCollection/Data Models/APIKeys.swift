99d17f514055019ea61dea5cfb5b1ef3e9766be5
Jonathon Lannon <jonathonlannon@icloud.com>
2021-08-15 16:00:21 -0400

    Add a "test" function to the code to read from the Bing Image Search api.

--- VideoGameCollection/Data Models/APIKeys.swift
+++ VideoGameCollection/Data Models/APIKeys.swift
@@ -9,3 +9,4 @@ import Foundation
 
 let rawgAPIKey = "3c7897d6c00a4f0fae76833a5c8e743c"
 let barcodeLookupAPIKey = "7lxxwer49f7yztkqs7b1bhr11u8i5a"
+let bingSearchAPIKey = "f4f54e9c46784565a831bb77da103d22"
