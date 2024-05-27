1b537bcb4cdad9bfd168eec4f6a85dff032059d8
Jon Iger <44842541+AppleMac1976@users.noreply.github.com>
2024-05-27 16:59:49 -0700

    Reorganization
    Reorg of project source code and assets to fit new project naming

---
+++ GameCollect/Data Models/APIKeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIKeys.swift
+//  VideoGameCollection
+//
+//  Created by Jon Iger on 7/28/21.
+//
+
+import Foundation
+
+let rawgAPIKey = "3c7897d6c00a4f0fae76833a5c8e743c"
+let barcodeLookupAPIKey = "7lxxwer49f7yztkqs7b1bhr11u8i5a"
+let bingSearchAPIKey = "f4f54e9c46784565a831bb77da103d22"
