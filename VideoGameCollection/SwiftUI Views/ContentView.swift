3f5ae280e040be4211bc668f9afbe28f94668645
Jon Iger <44842541+AppleMac1976@users.noreply.github.com>
2024-06-09 03:05:19 -0700

    Re model architecture of main Content View

--- VideoGameCollection/SwiftUI Views/ContentView.swift
+++ VideoGameCollection/SwiftUI Views/ContentView.swift
@@ -12,9 +12,14 @@ import CoreData
  View to display the user's current games in their collection
  */
 struct ContentView: View {
-    @State private var iCloudStatusAlert = false
-    @State private var iCloudStatusMessage = String()
+    @State var viewModel: ContentViewModel
+    private var iCloudStatusMessage = String()
+    
     //SwiftUI body
+    init() {
+        viewModel = ContentViewModel()
+    }
+    
     var body: some View {
         TabView{
             CollectionView()
@@ -36,44 +41,26 @@ struct ContentView: View {
                     Text("Settings")
                 }
         }
-        .alert(isPresented: $iCloudStatusAlert){
-            Alert(title: Text("iCloud Error"), message: Text(iCloudStatusMessage), primaryButton: Alert.Button.default(Text("OK")){
+        .alert(isPresented: $viewModel.iCloudStatusAlert){
+            Alert(title: Text("iCloud Error"), message: Text($viewModel.errorMessage.wrappedValue), primaryButton: Alert.Button.default(Text("OK")){
                 print("User ignored iCloud Warning")
             }, secondaryButton: Alert.Button.default(Text("Go to Settings")){
-                let application = UIApplication.shared
-                if let url = URL(string: UIApplication.openSettingsURLString), application.canOpenURL(url)    {
-                    application.open(url, options: [:], completionHandler: nil)
-                }
+                openPhoneSettings()
             })
         }
         .onAppear{
-            container.accountStatus(completionHandler: {status, error in
-                print("iCloud account status code \(status.rawValue)")
-                let accountStatusCode = status.rawValue
-                switch accountStatusCode {
-                    case 0:
-                        iCloudStatusMessage = "Could not determine status. Completion handler executed properly, but Apple could not determine the status."
-                        print("iCloud Account Status = 0")
-                    case 1:
-                        print("iCloud Account Status = 1")
-                    case 2:
-                        iCloudStatusMessage = "System restrictions have denied the user the ability to access iCloud on this device. Go to Settings to resolve the issue."
-                        print("iCloud Account Status = 2")
-                    case 3:
-                        iCloudStatusMessage = "No iCloud account is signed into on this device. Go to Settings to resolve this issue."
-                        print("iCloud Account Status = 3")
-                    case 4:
-                        iCloudStatusMessage = "iCloud is not available at this time. Could not load data. Try again later."
-                        print("iCloud Account Status = 4")
-                    default:
-                        iCloudStatusMessage = "Unknown error. Contact the developer for more support."
-                        print("iCloud Account Status could not be determined")
-                }
-                iCloudStatus = accountStatusCode
-                if accountStatusCode != 1{
-                    iCloudStatusAlert.toggle()
-                }
-            })
+            setupViewOnAppear()
+        }
+    }
+    
+    func setupViewOnAppear() {
+        viewModel.checkCloudStatus()
+    }
+    
+    func openPhoneSettings () {
+        let application = UIApplication.shared
+        if let url = URL(string: UIApplication.openSettingsURLString), application.canOpenURL(url)    {
+            application.open(url, options: [:], completionHandler: nil)
         }
     }
 }
--- VideoGameCollection/View Models/ContentViewModel.swift
+++ VideoGameCollection/View Models/ContentViewModel.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import CoreData
+import SwiftUI
+
+extension ContentView {
+    @Observable
+    class ContentViewModel {
+        private var iCloudStatusCode: Int = -1
+        public var iCloudStatusAlert = false
+        public var errorMessage: String = ""
+        enum errorCodes: String {
+            case noStatusDetermined = "Could not determine status."
+            case normalStatus = "iCloud Status Valid"
+            case iCloudAccessError = "System restrictions have denied the user the ability to access iCloud on this device. Go to Settings to resolve the issue."
+            case iCloudNotSignedIn = "No iCloud account is signed into on this device. Go to Settings to resolve this issue."
+            case iCloudNotAvailable = "iCloud is not available at this time. Try again later."
+            case unknownError = "Unknown error. Contact the developer for more support."
+        }
+        
+        func checkCloudStatus() {
+            container.accountStatus(completionHandler: {status, error in
+                print("iCloud account status code \(status.rawValue)")
+                let accountStatusCode = status.rawValue
+                switch accountStatusCode {
+                case 0:
+                    self.errorMessage = "Could not determine status. Completion handler executed properly, but Apple could not determine the status."
+                case 1:
+                    print("Successful!")
+                case 2:
+                    self.errorMessage = "System restrictions have denied the user the ability to access iCloud on this device. Go to Settings to resolve the issue."
+                case 3:
+                    self.errorMessage = "No iCloud account is signed into on this device. Go to Settings to resolve this issue."
+                case 4:
+                    self.errorMessage = "iCloud is not available at this time. Try again later."
+                default:
+                    self.errorMessage = "Unknown error. Contact the developer for more support."
+                    print("iCloud Account Status could not be determined")
+                }
+            })
+            if iCloudStatusCode != 1 {
+                iCloudStatusAlert.toggle()
+            }
+        }
+    }
+}
